c2394dc0a1fc160094552fb4ba14df9592efac66
Aleksandr Ivanov <aleksand7@yandex.ru>
2021-01-21 08:02:13 +0300

    funct component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
-import React, { Component } from 'react'
-export default class AddColorForm extends Component {
-  constructor(props) {
-    super(props);
+// import React, { Component } from 'react'
+import React, { useRef } from 'react'
 
-    this._title = React.createRef();
-    this._color = React.createRef();
+// export default class AddColorForm extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.submit = this.submit.bind(this);
-  }
+//     this._title = React.createRef();
+//     this._color = React.createRef();
 
-  submit(e) {
+//     this.submit = this.submit.bind(this);
+//   }
+
+//   submit(e) {
+//     e.preventDefault();
+
+//     const { _title: { current: title }, _color: { current: color} } = this;
+//     alert(`New Color ${ title.value } ${ color.value }`);
+//     title.value = '';
+//     color.value = '#000000';
+//     title.focus();
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.submit}>
+//         <input ref={this._title} type="text" placeholder="color title..." required/>
+//         <input ref={this._color} type="color" required/>
+//         <button>ADD</button>
+//       </form>
+//     )
+//   }
+// }
+
+
+const AddColorForm = ({ onNewColor = () => { } }) => {
+
+  const title = useRef(null);
+  const color = useRef(null);
+
+  const submit = e => {
     e.preventDefault();
 
-    const { _title: { current: title }, _color: { current: color} } = this;
-    alert(`New Color ${ title.value } ${ color.value }`);
-    title.value = '';
-    color.value = '#000000';
-    title.focus();
+    let _title = title.current;
+    let _color = color.current;
+
+    onNewColor(_title, _color);
+    _title.value = '';
+    _color.value = '#000000';
+    _title.focus();
   }
 
-  render() {
-    return (
-      <form onSubmit={this.submit}>
-        <input ref={this._title} type="text" placeholder="color title..." required/>
-        <input ref={this._color} type="color" required/>
-        <button>ADD</button>
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={submit}>
+      <input ref={title} type="text" placeholder="color title..." required />
+      <input ref={color} type="color" required />
+      <button>ADD</button>
+    </form>
+  )
 }
+
+export default AddColorForm;
