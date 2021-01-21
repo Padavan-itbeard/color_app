c502e1e321a575c390fb23593939b91de4fefa97
Aleksandr Ivanov <aleksand7@yandex.ru>
2021-01-21 07:11:53 +0300

    add form + refs

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
+import React, { Component } from 'react'
+export default class AddColorForm extends Component {
+  constructor(props) {
+    super(props);
 
+    this._title = React.createRef();
+    this._color = React.createRef();
+
+    this.submit = this.submit.bind(this);
+  }
+
+  submit(e) {
+    e.preventDefault();
+
+    const { _title: { current: title }, _color: { current: color} } = this;
+    alert(`New Color ${ title.value } ${ color.value }`);
+    title.value = '';
+    color.value = '#000000';
+    title.focus();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.submit}>
+        <input ref={this._title} type="text" placeholder="color title..." required/>
+        <input ref={this._color} type="color" required/>
+        <button>ADD</button>
+      </form>
+    )
+  }
+}
